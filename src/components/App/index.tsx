e3a59f6ff3fd4d322732e67eae260d8b5f43554c
RdotSilva <RyandotSilva@gmail.com>
2020-09-22 18:17:37 -0400

    Add NumberDisplay to main app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NumberDisplay from "../NumberDisplay";
 
 import "./App.scss";
 
@@ -6,6 +7,8 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className="Header"></div>
+      <NumberDisplay value={0} />
+      <NumberDisplay value={23} />
       <div className="Body"></div>
     </div>
   );
