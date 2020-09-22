e3a3a4c176bab53ff9bb9fd7eaa8f9298ed3decb
RdotSilva <RyandotSilva@gmail.com>
2020-09-22 18:15:46 -0400

    Add NumberDisplay component

---
+++ src/components/NumberDisplay/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./NumberDisplay.scss";
+
+const NumberDisplay: React.FC<NumberDisplayProps> = (props) => {
+  return <div className="NumberDisplay">000</div>;
+};
+
+export default NumberDisplay;
