b95ec7b87538606535e51c3c2409fb9d7a2a9504
RdotSilva <RyandotSilva@gmail.com>
2020-09-24 19:38:24 -0400

    Add renderCells method

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import { generateCells } from "../../utils";
 import NumberDisplay from "../NumberDisplay";
-
 import "./App.scss";
+import Button from "./../Button/index";
 
 const App: React.FC = () => {
   const [cells, setCells] = useState(generateCells());
 
-  console.log("cells", cells);
+  const renderCells = (): React.ReactNode => {
+    return cells.map((row, rowIndex) =>
+      row.map((cell, colIndex) => <Button key={`${rowIndex}-${colIndex}`} />)
+    );
+  };
+
   return (
     <div className="App">
       <div className="Header">
@@ -19,7 +24,7 @@ const App: React.FC = () => {
         </div>
         <NumberDisplay value={23} />
       </div>
-      <div className="Body">Body</div>
+      <div className="Body">{renderCells()}</div>
     </div>
   );
 };
