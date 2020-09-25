63de9971774e63599cfbb982fa17ff7437035c12
RdotSilva <RyandotSilva@gmail.com>
2020-09-25 18:16:46 -0400

    Add ButtonProps interface

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { CellState, CellValue } from "../../types";
 import "./Button.scss";
 
-const Button: React.FC = () => {
+interface ButtonProps {
+  row: number;
+  col: number;
+  state: CellState;
+  value: CellValue;
+}
+
+const Button: React.FC<ButtonProps> = () => {
   return <div className="Button"></div>;
 };
 
