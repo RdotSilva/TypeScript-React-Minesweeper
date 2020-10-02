960042eeacf2da42c15cff96ed6a864524774a93
RdotSilva <RyandotSilva@gmail.com>
2020-10-02 18:48:36 -0400

    Add JSDoc comments

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Represents the value of a cell
+ * @export
+ * @enum {number}
+ */
 export enum CellValue {
   None,
   One,
@@ -11,17 +16,27 @@ export enum CellValue {
   Bomb,
 }
 
+/**
+ * Represents the state of a cell
+ * @export
+ * @enum {number}
+ */
 export enum CellState {
   Open,
   Visible,
   Flagged,
 }
 
-export type Cell = { value: CellValue; state: CellState };
-
+/**
+ * Represents difference faces used throughout game
+ * @export
+ * @enum {number}
+ */
 export enum Face {
   Smile = "🙂",
   Oh = "😟",
   Lost = "💀",
   Won = "😎",
 }
+
+export type Cell = { value: CellValue; state: CellState };
