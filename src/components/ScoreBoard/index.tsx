02103d7a58f3e7084a77538612847f828a370380
RdotSilva <RyandotSilva@gmail.com>
2020-10-05 19:06:05 -0400

    Add ScoreBoard component

---
+++ src/components/ScoreBoard/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./ScoreBoard.scss";
+
+const ScoreBoard: React.FC = () => {
+  return (
+    <div>
+      <div className="Top">Top Player</div>
+      <div className="Bottom">Bottom Player</div>;
+    </div>
+  );
+};
+
+export default ScoreBoard;
