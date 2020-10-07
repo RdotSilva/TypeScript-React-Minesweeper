2d2aa8a9b8a1fcaa52af384f82733668eee4958c
RdotSilva <RyandotSilva@gmail.com>
2020-10-07 19:34:13 -0400

    Add difficulty buttons

--- src/components/GameSettings/index.tsx
+++ src/components/GameSettings/index.tsx
@@ -3,7 +3,18 @@ import React from "react";
 import "./GameSettings.scss";
 
 const GameSettings: React.FC = () => {
-  return <div className="GameSettings"></div>;
+  return (
+    <div className="GameSettings">
+      <div className="TopBar">
+        <p>Height</p>
+        <p>Width</p>
+        <p>Mines</p>
+      </div>
+      <button className="DifficultyButton">Beginner</button>
+      <button className="DifficultyButton">Intermediate</button>
+      <button className="DifficultyButton">Advanced</button>
+    </div>
+  );
 };
 
 export default GameSettings;
