0e16fc25e53055c0088f0ae26eeb1e2d151072a4
RdotSilva <RyandotSilva@gmail.com>
2020-10-07 19:42:30 -0400

    Add state for initial settings

--- src/components/GameSettings/index.tsx
+++ src/components/GameSettings/index.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./GameSettings.scss";
 
 const GameSettings: React.FC = () => {
+  const [width, setWidth] = useState<number>(9);
+  const [height, setHeight] = useState<number>(9);
+  const [mines, setMines] = useState<number>(10);
+
   return (
     <div className="GameSettings">
       <div className="TopBar">
-        <p>Height</p>
-        <p>Width</p>
-        <p>Mines</p>
+        <p>Height: {height}</p>
+        <p>Width: {width}</p>
+        <p>Mines: {mines}</p>
       </div>
       <button className="DifficultyButton">Beginner</button>
       <button className="DifficultyButton">Intermediate</button>
