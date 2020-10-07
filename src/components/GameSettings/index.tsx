2b58c38b1c7824c1796acd517cb03dd6042819e7
RdotSilva <RyandotSilva@gmail.com>
2020-10-07 19:31:55 -0400

    Add GameSettings component

---
+++ src/components/GameSettings/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./GameSettings.scss";
+
+const GameSettings: React.FC = () => {
+  return <div className="GameSettings"></div>;
+};
+
+export default GameSettings;
