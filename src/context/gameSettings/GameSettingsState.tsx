049df2e3bba96e8ef5b1bec88dc3e54f237fff02
RdotSilva <RyandotSilva@gmail.com>
2020-10-08 19:35:54 -0400

    Add TODOs to implement dispatch/provider

--- src/context/gameSettings/GameSettingsState.tsx
+++ src/context/gameSettings/GameSettingsState.tsx
@@ -1,4 +1,6 @@
 import React, { useReducer } from "react";
+import { CHANGE_DIFFICULTY } from "../types";
+import gameSettingsReducer from "./gameSettingsReducer";
 
 const GameSettingsState = (props) => {
   const initialState = {
@@ -6,4 +8,8 @@ const GameSettingsState = (props) => {
     height: 9,
     mines: 10,
   };
+
+  // TODO: Add dispatch for CHANGE_DIFFICULTY
+
+  // TODO: Return GameSettingsContext.Provider
 };
