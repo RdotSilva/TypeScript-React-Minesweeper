b8cd95d3f8f4ddaedba97d7878e21c47d8dc18b4
RdotSilva <RyandotSilva@gmail.com>
2020-10-09 19:40:43 -0400

    Add GameBoard component

---
+++ src/components/GameBoard/GameBoard.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useContext } from "react";
+
+import "./GameBoard.scss";
+
+const GameBoard: React.FC = () => {
+  return <div className="GameSettings"></div>;
+};
+
+export default GameBoard;
