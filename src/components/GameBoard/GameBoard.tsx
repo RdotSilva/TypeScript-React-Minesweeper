56be428fadd576d61d69791a96f60a00c5129279
RdotSilva <RyandotSilva@gmail.com>
2020-10-10 19:13:37 -0400

    Add grabAllAdjacentCells method

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import gameSettingsContext from "../../context/gameSettings/gameSettingsContext";
 import { Cell, CellState, CellValue } from "../../types";
+import { CellObject } from "./../../interfaces/CellObject";
 
 import "./GameBoard.scss";
 
@@ -8,7 +9,43 @@ const GameBoard: React.FC = () => {
   const context = useContext(gameSettingsContext);
   const { height, width, mines } = context;
 
-  // TODO: Move grabAdjacentCells method
+  const grabAllAdjacentCells = (
+    cells: Cell[][],
+    rowParam: number,
+    colParam: number
+  ): CellObject => {
+    const topLeftCell =
+      rowParam > 0 && colParam > 0 ? cells[rowParam - 1][colParam - 1] : null;
+    const topCell = rowParam > 0 ? cells[rowParam - 1][colParam] : null;
+    const topRightCell =
+      rowParam > 0 && colParam < width - 1
+        ? cells[rowParam - 1][colParam + 1]
+        : null;
+    const leftCell = colParam > 0 ? cells[rowParam][colParam - 1] : null;
+    const rightCell =
+      colParam < width - 1 ? cells[rowParam][colParam + 1] : null;
+    const bottomLeftCell =
+      rowParam < height - 1 && colParam > 0
+        ? cells[rowParam + 1][colParam - 1]
+        : null;
+    const bottomCell =
+      rowParam < height - 1 ? cells[rowParam + 1][colParam] : null;
+    const bottomRightCell =
+      rowParam < height - 1 && colParam < width - 1
+        ? cells[rowParam + 1][colParam + 1]
+        : null;
+
+    return {
+      topLeftCell,
+      topCell,
+      topRightCell,
+      leftCell,
+      rightCell,
+      bottomLeftCell,
+      bottomCell,
+      bottomRightCell,
+    };
+  };
 
   const generateCells = (): Cell[][] => {
     let cells: Cell[][] = [];
