25735bfafb2e5cb77fcc845d3b3a1be3610b92e9
RdotSilva <RyandotSilva@gmail.com>
2020-10-10 19:12:29 -0400

    Add CellObject interface

---
+++ src/interfaces/CellObject.ts
@@ -0,0 +1,12 @@
+import { Cell } from "../types/index";
+
+export interface CellObject {
+  topLeftCell: Cell | null;
+  topCell: Cell | null;
+  topRightCell: Cell | null;
+  leftCell: Cell | null;
+  rightCell: Cell | null;
+  bottomLeftCell: Cell | null;
+  bottomCell: Cell | null;
+  bottomRightCell: Cell | null;
+}
